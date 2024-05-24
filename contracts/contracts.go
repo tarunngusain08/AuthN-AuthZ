1fba0fad915cc922aa0e8c2fe2e457ffed947e2c
Tarunn Gusain <36428256+tarunngusain08@users.noreply.github.com>
2024-05-24 19:34:06 +0530

    Create contracts.go

---
+++ contracts/contracts.go
@@ -0,0 +1,7 @@
+package contracts
+
+type RegisterUser struct {
+	Username string `json:"username" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
+}
