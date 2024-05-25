3dc4c27d7a6061f4693b5e3e78459887fa0ea859
Tarunn Gusain <36428256+tarunngusain08@users.noreply.github.com>
2024-05-25 13:18:06 +0530

    Update main.go

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"AuthN-AuthZ/repo"
 	"github.com/gin-gonic/gin"
 	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 )
 
 type Handlers struct {
@@ -17,13 +18,15 @@ func main() {
 	router := gin.Default()
 	router.POST("/api/register", Handler.RegisterUser.RegisterUser)
 	router.POST("api/login")
+	router.Run(":8080")
 }
 
 func init() {
-	db, err := sqlx.Connect("postgres", "user=user123 dbname=AuthN-AuthZ password=1234")
+	db, err := sqlx.Connect("postgres", "dbname=postgres host=localhost port=5432 sslmode=disable")
 	if err != nil {
 		panic(err)
 	}
+	Handler = new(Handlers)
 	registerRepo := repo.NewRegisterUserRepo(db)
 	registerUser := handlers.NewRegisterUserHandler(registerRepo)
 	Handler.RegisterUser = registerUser
