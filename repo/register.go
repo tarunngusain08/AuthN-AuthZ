b26df691fc7b1af6946901009571c83908dcecbf
Tarunn Gusain <36428256+tarunngusain08@users.noreply.github.com>
2024-05-25 12:41:14 +0530

    Create register.go

---
+++ repo/register.go
@@ -0,0 +1,42 @@
+package repo
+
+import (
+	"AuthN-AuthZ/contracts"
+	"AuthN-AuthZ/models"
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type RegisterUserRepo struct {
+	db *sqlx.DB
+}
+
+func NewRegisterUserRepo(db *sqlx.DB) *RegisterUserRepo {
+	return &RegisterUserRepo{
+		db: db,
+	}
+}
+
+func (r *RegisterUserRepo) Register(userDetails *contracts.RegisterUser) error {
+	// Hash the password
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(userDetails.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return fmt.Errorf("failed to hash password: %v", err)
+	}
+
+	user := models.User{
+		Username: userDetails.Username,
+		Email:    userDetails.Email,
+		Password: string(hashedPassword),
+	}
+
+	// Insert user into database
+	query := `INSERT INTO users (username, email, password) VALUES (:username, :email, :password)`
+	_, err = r.db.NamedExec(query, &user)
+	if err != nil {
+		return fmt.Errorf("failed to insert user: %v", err)
+	}
+
+	return nil
+}
