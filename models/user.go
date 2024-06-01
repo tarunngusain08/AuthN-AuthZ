003a86206faba0e01bbfc2ee88c4aedb61245986
Tarunn Gusain <36428256+tarunngusain08@users.noreply.github.com>
2024-06-01 18:52:34 +0530

    Update user.go

--- models/user.go
+++ models/user.go
@@ -1,8 +1,11 @@
 package models
 
+import "time"
+
 type User struct {
-	ID       int    `db:"id"`
-	Username string `db:"username"`
-	Email    string `db:"email"`
-	Password string `db:"password"`
+	ID        int        `db:"id"`
+	Username  string     `db:"username"`
+	Email     string     `db:"email"`
+	Password  string     `db:"password"`
+	CreatedAt *time.Time `db:"created_at"`
 }
