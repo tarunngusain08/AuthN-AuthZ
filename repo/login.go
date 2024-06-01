b23e25d84f2dd2762d7e97c5d2874b827a352cc8
Tarunn Gusain <36428256+tarunngusain08@users.noreply.github.com>
2024-06-01 19:06:25 +0530

    Update login.go

--- repo/login.go
+++ repo/login.go
@@ -3,30 +3,55 @@ package repo
 import (
 	"AuthN-AuthZ/contracts"
 	"AuthN-AuthZ/models"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/jmoiron/sqlx"
 	"golang.org/x/crypto/bcrypt"
+	"time"
 )
 
 type LoginRepo struct {
-	db *sqlx.DB
+	db         *sqlx.DB
+	signingKey []byte
 }
 
-func NewLoginRepo(db *sqlx.DB) *LoginRepo {
-	return &LoginRepo{db: db}
+func NewLoginRepo(db *sqlx.DB, signingKey []byte) *LoginRepo {
+	return &LoginRepo{db: db, signingKey: signingKey}
 }
 
-func (l *LoginRepo) Login(userDetails *contracts.Login) error {
+func (l *LoginRepo) Login(userDetails *contracts.LoginRequest) (*contracts.LoginResponse, error) {
 	fetchedUserDetails := new(models.User)
 	query := `SELECT * FROM Users WHERE Email = $1`
 
 	err := l.db.Get(fetchedUserDetails, query, userDetails.Email)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	err = bcrypt.CompareHashAndPassword([]byte(fetchedUserDetails.Password), []byte(userDetails.Password))
 	if err != nil {
-		return err
+		return nil, err
 	}
-	return nil
+	token, err := l.generateToken(userDetails)
+	if err != nil {
+		return nil, err
+	}
+	return token, nil
+}
+
+func (l *LoginRepo) generateToken(userDetails *contracts.LoginRequest) (*contracts.LoginResponse, error) {
+	// Define the token claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"authorized": true,
+		"email":      userDetails.Email,
+		"password":   userDetails.Password,
+		"exp":        time.Now().Add(time.Hour * 24).Unix(), // Token expires in 24 hours
+	})
+
+	// Sign the token with the secret key
+	tokenString, err := token.SignedString(l.signingKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return &contracts.LoginResponse{Token: tokenString}, nil
 }
