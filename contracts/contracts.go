a88b4398cc783c9ab0148da5e753b48520117305
Tarunn Gusain <36428256+tarunngusain08@users.noreply.github.com>
2024-06-01 19:06:35 +0530

    Update contracts.go

--- contracts/contracts.go
+++ contracts/contracts.go
@@ -6,7 +6,11 @@ type Register struct {
 	Password string `json:"password" binding:"required"`
 }
 
-type Login struct {
+type LoginRequest struct {
 	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required"`
+}
+
+type LoginResponse struct {
+	Token string `json:"token"`
 }
